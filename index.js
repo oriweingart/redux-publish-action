82b1dbb5832558675d38004171a656b60421f9eb
ori weingart <oweingart@forter.com>
2016-04-04 18:11:33 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,142 @@
+"use strict";
+
+import PUBNUB from 'pubnub';
+
+const channel = 'PUBLISH_ACTIONS';
+/**
+ * Wrapper to store actions and send them
+ */
+class PubActions {
+    constructor(pubnub) {
+        this.actions = [];
+        this.pubnub = pubnub;
+    }
+    add(action) {
+        this.actions.push({id: this.actions.length+1, action});
+        return this;
+    }
+    send() {
+        let message = JSON.stringify([this.actions[this.actions.length-1]]);
+        this.pubnub.publish({channel, message});
+        return this;
+    }
+    sendAll() {
+        let message = JSON.stringify(this.actions);
+        this.pubnub.publish({channel, message});
+        return this;
+    }
+}
+/**
+ * Middleware that publish it's new action to the receivers
+ * @returns {Function}
+ */
+const sendActionMiddleware = (pubnub) => {
+    const pubActions = new PubActions(pubnub);
+    // When a new member joined the channel -> publish all actions
+    pubnub.subscribe({ channel,
+        presence: (m)=>{
+            if (m.action === 'join') {
+                pubActions.sendAll();
+            }
+        },
+        message: ()=> {/* Do nothing*/ }
+    });
+    return store => {
+        return next => action => {
+            let result = next(action);
+            pubActions.add(action).sendAll();
+            return result;
+        };
+    }
+}
+/**
+ * Middleware that:
+ *      1. Block action from current user
+ *      2. Continue to next action only when it was recived from sender
+ * @returns {Function}
+ */
+const receiveActionMiddleware = (pubnub) => {
+    let mainStore,
+        curActionId = 0;
+    pubnub.subscribe({
+        channel,
+        message : message  => {
+            message = JSON.parse(message);
+            if (Array.isArray(message)) {
+                message.map(actionObj => {
+                    if (shouldAddAction(actionObj)) {
+                        addAction(actionObj);
+                    }
+                })
+            }
+        }
+    })
+    const shouldAddAction = message => message && message.id > curActionId;
+    const addAction       = message => {
+        message.action.force = true;
+        mainStore.dispatch(message.action);
+        curActionId = message.id;
+    }
+    return store => {
+        mainStore = mainStore || store; // init main store
+        return next => action => {
+            if (!action.force) {
+                return action; // Block action change
+            } else {
+                return next(action);
+            }
+        };
+    }
+}
+/**
+ * Middleware that doesnt do anything (in case of regular user)
+ * @returns {Function}
+ */
+const farwardNextAction = () => {
+    return store => {
+        return next => action => {
+            let result = next(action);
+            return result;
+        };
+    }
+}
+/**
+ * Helper to extract query param from url
+ * @param field
+ * @param url
+ * @returns {null}
+ */
+const getQueryString = function (field) {
+    let href        = window.location.href,
+        reg         = new RegExp('[?&]' + field + '=([^&#]*)', 'i'),
+        string      = reg.exec(href);
+    return string ? string[1] : null;
+};
+/**
+ * Is this a sender user
+ * @returns {null}
+ */
+const isSender = (userType) => userType ? userType == 'sender' : getQueryString('sender');
+/**
+ * Is this a receiver user
+ * @returns {null}
+ */
+const isReceiver = (userType) => userType ? userType == 'receiver' : getQueryString('receiver');
+/**
+ * Return the middleware based on the url query param:
+ *  url?sender=true -> will send actions to others
+ *  url?receiver=true -> will block interaction and get actions from sender
+ *  otherwise -> won't do anything
+ * @returns {Function}
+ */
+export default (publish_key, subscribe_key, userType) => {
+    let uuid = new Date().getTime();
+    const pubnub = PUBNUB.init({publish_key, subscribe_key, uuid});
+    if (isSender(userType)) {
+        return sendActionMiddleware(pubnub);
+    }
+    if (isReceiver(userType)) {
+        return receiveActionMiddleware(pubnub);
+    }
+    return farwardNextAction();
+}
---
+++ test/publish.action.test.js
@@ -0,0 +1,49 @@
+import chai from 'chai';
+import configureStore from 'redux-mock-store';
+import publishActionsMiddleware from '../index';
+
+
+describe('publishActionsMiddleware middleware', () => {
+    const test_keys           = ['pub-c-6892057c-e765-4d44-b5f9-26220865480d', 'sub-c-6c1c3c02-fa52-11e5-9086-02ee2ddab7fe'];
+    const createMockStore = (userType) => {
+        const middlewares = [publishActionsMiddleware(...test_keys, userType)];
+        return configureStore(middlewares);
+    }
+    const mockSenderAction = {
+        type: 'SENDER_ADD_ITEM',
+        payload: {
+            text: "Color TV"
+        }
+    };
+    const mockReceiverAction = {
+        type: 'SENDER_ADD_ITEM',
+        payload: {
+            text: "Color TV"
+        }
+    };
+
+    describe('test publish store with one sender, one receiver and one regular user', () => {
+        const senderStore    = createMockStore('sender')({});
+        const receiverStore  = createMockStore('receiver')({});
+        const regulatStore   = createMockStore('none')({});
+
+
+        it('should block receiver from dispatching actions', ()  => {
+            receiverStore.dispatch(mockReceiverAction);
+            const actions = receiverStore.getActions();
+            chai.assert.strictEqual(actions.length, 0);
+        });
+
+        it('should send senders action to receiver ', (done)  => {
+            senderStore.dispatch(mockSenderAction);
+            let actions = receiverStore.getActions();
+            chai.assert.strictEqual(actions.length, 0);
+            setTimeout(() => {
+                actions = receiverStore.getActions();
+                chai.assert.strictEqual(actions.length, 1);
+                chai.assert.strictEqual(actions[0].force, true);
+                done();
+            }, 2000)
+        });
+    });
+});
